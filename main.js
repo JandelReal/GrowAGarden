53699a83456618b9cdde91e229e45ffe0bcb315b
JandelReal <beluga69420123@gmail.com>
2025-08-01 16:34:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // 🔥 Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBO3h_iI7SHhZFytdHc3nKhddlxAPKwRIs",
+  authDomain: "grow-a-garden-dfdab.firebaseapp.com",
+  databaseURL: "https://grow-a-garden-dfdab-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "grow-a-garden-dfdab",
+  storageBucket: "grow-a-garden-dfdab.firebasestorage.app",
+  messagingSenderId: "868567449799",
+  appId: "1:868567449799:web:b7e28d1e2ff9c7ce53bee2"
 };
 
 firebase.initializeApp(firebaseConfig);
